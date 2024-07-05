603d97512acb8213ac2473d75cf9b6e4a421fa93
Cesar Fernandez <CesarFernandez2@live.com>
2024-07-05 15:15:23 -0600

    Initial Commit

---
+++ API Project Part 2/API_Project_Part_2App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Project_Part_2App.swift
+//  API Project Part 2
+//
+//  Created by Cesar Fernandez on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct API_Project_Part_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
